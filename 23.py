c4c88ed116f1c782817864b3a92ac22b365a03ee
Redvarik <104436331+Redvarik@users.noreply.github.com>
2022-10-05 21:00:07 +0300

    Update 23.py

--- 23.py
+++ 23.py
@@ -4,7 +4,7 @@ from scipy.stats import itemfreq
 
 
 def get_dominant_color(image, n_colors):
-    pixels = np.float32(image).reshape((-1, 3))  # reshape придает массиву новую форму без изменения его данных.
+    pixels = np.float32(image).reshape((-1, 3))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
     flags = cv2.KMEANS_RANDOM_CENTERS
     flags, labels, centroids = cv2.kmeans(
